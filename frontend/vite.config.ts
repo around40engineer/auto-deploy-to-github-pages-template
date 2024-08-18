da5356877edee9f990afab653ca91fde0c313cf2
around40engineer <around40engineer@gmail.com>
2024-08-19 00:32:40 +0900

    ver2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd())
+    process.env = {...process.env,...loadEnv(mode, process.cwd())}
 
     return {
         plugins: [react()],
-        base: env.REPOSITORY_NAME?`/${env.REPOSITORY_NAME}/`:'/',
+        base: process.env.VITE_REPOSITORY_NAME?`/${process.env.VITE_REPOSITORY_NAME}/`:'/',
         test: {
             globals: true,
             environment: 'jsdom',
