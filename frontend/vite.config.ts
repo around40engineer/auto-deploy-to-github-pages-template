7a9c33464af24f1658b2870a8c7b6470ad955a35
around40engineer <around40engineer@gmail.com>
2024-08-18 19:27:14 +0900

    vite.config.tsで環境変数を読み込めるように変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: import.meta.env.GITHUB_ACTIONS ? '/auto-deploy-to-github-pages-template/' : './',
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./src/setup.ts']
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    const  VITE_GITHUB_ACTIONS  = `${env.VITE_GITHUB_ACTIONS ?? false}`
+
+    return {
+        plugins: [react()],
+        base: VITE_GITHUB_ACTIONS==='true' ? '/auto-deploy-to-github-pages-template/' : '/',
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: ['./src/setup.ts']
+        },
+    }
 })
