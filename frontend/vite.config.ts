c2470a139f2a63e5b1ad64cad1a6cb742e36c5bb
around40engineer <around40engineer@gmail.com>
2024-08-18 23:18:18 +0900

    vite.config.tsをrepository nameを取得する方法に変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-    // const env = loadEnv(mode, process.cwd())
-    // const  VITE_GITHUB_ACTIONS  = `${env.VITE_GITHUB_ACTIONS ?? false}`
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd())
 
     return {
         plugins: [react()],
-        base: '/auto-deploy-to-github-pages-template/',
+        base: env.REPOSITORY_NAME?`/${env.REPOSITORY_NAME}/`:'/',
         test: {
             globals: true,
             environment: 'jsdom',
