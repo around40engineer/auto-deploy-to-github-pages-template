874f3829856c7e8f17ba9fea193e23b3cd5ed981
around40engineer <around40engineer@gmail.com>
2024-08-18 18:36:07 +0900

    vite.config.tsのprocessをimport.metaに変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: process.env.GITHUB_ACTIONS ? '/auto-deploy-to-github-pages-template/' : './',
+    base: import.meta.env.GITHUB_ACTIONS ? '/auto-deploy-to-github-pages-template/' : './',
     test: {
         globals: true,
         environment: 'jsdom',
