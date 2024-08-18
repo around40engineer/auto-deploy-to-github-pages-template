762816cf39e380e6a640317c77259a0d096eb699
around40engineer <around40engineer@gmail.com>
2024-08-18 20:06:38 +0900

    App.tsxにconsoleを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 
 export const App = () => {
+
+    console.log('hello world!!!')
+    console.log(import.meta.env.VITE_GITHUB_ACTIONS)
+
     return (
         <div>
             hello world!!!
