83d80c3403a3c740b14a6747570282f07332c2c1
around40engineer <around40engineer@gmail.com>
2024-08-18 19:55:31 +0900

    vite.config.tsのbaseの設定をdefaultに戻す

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig ,loadEnv} from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd())
-    const  VITE_GITHUB_ACTIONS  = `${env.VITE_GITHUB_ACTIONS ?? false}`
+export default defineConfig(() => {
+    // const env = loadEnv(mode, process.cwd())
+    // const  VITE_GITHUB_ACTIONS  = `${env.VITE_GITHUB_ACTIONS ?? false}`
 
     return {
         plugins: [react()],
-        base: VITE_GITHUB_ACTIONS==='true' ? '/auto-deploy-to-github-pages-template/' : '/',
+        // base: VITE_GITHUB_ACTIONS==='true' ? '/auto-deploy-to-github-pages-template/' : '/',
         test: {
             globals: true,
             environment: 'jsdom',
